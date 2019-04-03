81134dc4edf4b58fbb7742ae236536d2d6f45d13
Lukasz_Witek_vel_Witkowski <uwlwitkowski@gmail.com>
2019-04-03 17:28:26 +0200

    Łukasz Witek vel Witkowski
    Przygotowanie programu do czwartych zajęć z grupy podstawowej cpp

--- Zadanie_3/Zadanie_3.cpp
+++ Zadanie_3/Zadanie_3.cpp
@@ -1,5 +1,11 @@
+//--------------------------------------------------------------------------------------------------------------//
+//	date: 03-04-2019 															Author: Grupa Podstawowa Cpp	//
+//	name: "Gra w Kolko i krzyzyk"																				//
+//	title: "Gra z mozliwoscia ustalenia wielkosci planszy wraz z regula, ile znakow kolo siebie trzeba umiescic //
+//	by wygrac"																									//
+//--------------------------------------------------------------------------------------------------------------//
 #include <iostream>
-enum Program {
+enum Program { //typ enumeryczny 
 	MainMenu,
 	MenuSize,
 	MenuChoice,
@@ -11,13 +17,23 @@ enum Program {
 	Defeat,
 	Exit
 };
+//--------------------Procedury-------------------------------------------------------------------//
+void prMainMenu(Program& program, int& choice, int& value, bool reply); //procedura menu glownego
+void prSizeMenu(Program& program, int& size, int& length, int& value, bool reply); //procedura menu rozmiaru
+void prChoice(Program& program, int& choice, bool& wheel, int& value, bool reply); //procedura menu wyboru
+void prVictoryMenu(Program& program, int& choice, int& value, bool reply); //procedura menu zwyciestwa
+void prDefeatMenu(Program& program, int& choice, int& value, bool reply); //procedura menu porazki
+void prExit(bool& play); //procedura wyjscia z programu
+
+
+//----------------------------Pozostale funkcje---------------------------------------------------//
 bool mainSelection(int choice,int& value, Program& program); //Sprawdzenie wyboru dla Menu Glownego yes
 bool sizeSelection(int size,int length,int& value,Program& program); //Sprawdzenie wprowadzonych danych (rozmiar, dlugosc) yes
 bool victorySelection(int choice,int& value, Program& program); //Sprawdzenie wyboru dla Menu Zwyciestwa yes
 bool defeatSelection(int choice,int& value, Program& program); //Sprawdzenie wyboru dla Menu Przegranej yes
 bool charSelection(int choice, bool& wheel, Program& program); //Sprawdzenie wyboru dla Menu Wyboru znaku yes
-void constructor(char** board, int size); //Funkcja budujaca plansze yes
-void destroyer(char** board, int size); //Funkcja niszczaca plansze yes
+char** constructor(char** board, int size); //Funkcja budujaca plansze yes
+char** destroyer(char** board, int size); //Funkcja niszczaca plansze yes
 void switchProgram(int& value, Program& program); //funkcja przelaczajaca pomiedzy case w grze yes 
 void settingDefoult(int& size, int& lenght); //Ladowanie podstawowych danych yes
 void horizontalChecking(char** board,int size, int length, int& result); //Sprawdzanie poziome planszy no
@@ -26,100 +42,52 @@ void obliquecheck(char** board,int size, int length, int& result); //Spwardzanie
 void viewBoard(char** board, int size); //wyswietlenie planszy no
 bool trafficChoice(char**board, int size, int& choiceX, int& choiceY); //Pobranie danych odnosnie wybranej pozycji yes
 int main() {
-	int choice;
-	int value = 0;
-	int length;
-	int size;
-	bool wheel;
-	bool reply;
-	bool play = true;
-	char** board = nullptr;
-	Program program = Program::MainMenu;
+	int choice; //zmienna wyboru dla menu
+	int value; //zmienna pomocnicza w sterowaniu 
+	int length; //zmienna okreslajaca wartosc reguly
+	int size;	//zmienna okreslajaca wielkosc wielkosc planszy
+	bool wheel; //zmienna znaku
+	bool reply = false; //zmienna sterujaca odpowiedzialna za niepoprawne wprowadzenie wartosci
+	bool play = true; //zmienna okresljaca czy gra sie toczy, podstawowo utawiono na "true"
+	char** board = nullptr; //zmienna dotyczaca planszy
+	Program program = Program::MainMenu; //zmienna tytpu enumerowanego
 	do {
 		switch (program) {
-		case MainMenu:
-			reply = false;
-			do {
-				system("cls");
-				if (reply)
-					std::cout << "Podano niewlasciwe dane!\n";
-				std::cout <<"|---Menu Glowne---|\n"
-					<< "1.\tGra w kolko i krzyzyk\n"
-					<< "2.\tWlasne ustawienia gry\n"
-					<< "3.\tWyjscie\n"
-					<<"Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
-				std::cin >> choice;
-				reply = true;
-			} while (mainSelection(choice,value, program));break;
-		case MenuSize: 
-			reply = false;
-			do {
-				system("cls");
-				if (reply)
-					std::cout << "Podano niewlasciwe dane!\n";
-				std::cout << "|---Wybor opcji gry---|\n"
-					<< "Podaj rozmiar planszy: ";
-				std::cin >> size;
-					std::cout<< "Podaj jak dluga linie trzeba ulozyc: ";
-				std::cin >> length;
-				reply = true;
-			} while (sizeSelection(size,length,value,program)); break;
-		case MenuChoice: 	
-			reply = false;
-			do {
-				system("cls");
-				if (reply)
-					std::cout << "Podano niewlasciwe dane!\n";
-				std::cout<<"|--Menu wyboru znaku--|\n"
-					<<"1.\tO\n"
-					<<"2.\tX\n"
-					<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
-				std::cin >> choice;
-				reply = true;
-		} while (charSelection(choice, wheel,program)); break;
-		case SettingDefault:
+		case MainMenu:	prMainMenu(program, choice, value, reply); break;
+		case MenuSize: prSizeMenu(program, size, length, value, reply); break;
+		case MenuChoice: prChoice(program, choice, wheel, value, reply); break;
+		case SettingDefault: 	
 			settingDefoult(size, length);
 			switchProgram(value, program); break;
 		case Config: 
-			constructor(board, size); 
+			board = constructor(board, size); 
 			switchProgram(value, program); break;
-		case Play: break;
-		case Victory:
-			reply = false;
-			do {
-				system("cls");
-				if (reply)
-				std::cout << "Podano niewlasciwe dane!\n";
-				std::cout<<"Zwyciestwo!!\n"
-					<<"1.\tGraj jeszcze raz\n"
-					<<"2.\tWroc do Menu Glownego\n"
-					<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
-				reply = true;
-				std::cin >> choice;
-		} while (victorySelection(choice, value ,program)); break;
-		case Defeat:
-			reply = false;
-			do {
-				system("cls");
-				if (reply)
-					std::cout << "Podano niewlasciwe dane!\n";
-				std::cout << "Przegrales!!\n"
-					<< "1.\tGraj jeszcze raz\n"
-					<< "2.\tWroc do Menu Glownego\n"
-					<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
-				reply = true;
-				std::cin >> choice;
-			} while (defeatSelection(choice, value ,program));		break;
+		case Play: //do dokonczenia!!!
+			
+			
+			
+			
+			break;
+		case Victory:prVictoryMenu(program, choice, value, reply); break;
+		case Defeat: prDefeatMenu(program, choice, value, reply); break;
 		case Destroyer: 
-			destroyer(board,size); 
+			board = destroyer(board,size); 
 			switchProgram(value, program); break;
-		case Exit:
-			std::cout << "Wyjscie\n"; 
-			play = false; break;
+		case Exit:prExit(play);	break;
 		}
 	} while (play);
 	system("pause");
 	return 0;
+}
+void horizontalChecking(char** board, int size, int length, int& result) {
+
+}
+void verticalChecking(char** board, int size, int length, int& result) {
+
+}
+void obliquecheck(char** board, int size, int length, int& result) {
+
+
 }
 bool mainSelection(int choice,int& value, Program& program) {
 	switch (choice) {
@@ -175,21 +143,25 @@ bool charSelection(int choice, bool& wheel, Program& program) {
 	program = Program::Play;
 	return false;
 }
-void constructor(char** board, int size) {
+char** constructor(char** board, int size) {
 	int i;
 	int j;
 	board = new char*[size];
 	for (i = 0; i < size; i++) {
 		board[i] = new char[size];
 		for (j = 0; j < size; j++)
-			board[i][j] = ' ';
+			board[i][j] = 'X';
 	}
+	board[0][1] = 'O';
+	board[1][1] = ' ';
+	return board;
 }
-void destroyer(char** board, int size) {
+char** destroyer(char** board, int size) {
 	int i;
 	for (i = 0; i < size; i++)
 		delete[] board[i];
 	delete[] board;
+	return board;
 }
 void switchProgram(int& value, Program& program) {
 	switch (value) {
@@ -206,32 +178,103 @@ void settingDefoult(int& size, int& lenght) {
 	size = 3;
 	lenght = 3;
 }
-void horizontalChecking(char** board, int size, int length, int& result) {
-
-}
-void verticalChecking(char** board, int size, int length, int& result) {
-
-}
-void obliquecheck(char** board, int size, int length, int& result) {
-
-
-}
 void viewBoard(char** board, int size) {
 	int i;
-	int j;
-	for (i = 0; i < size; i++) {
-		if(i==0)
-		std::cout << (char)179;
-		for (j = 0; j < size-2; j++) {
-			std::cout << (char)148;
+	int t;
+	std::cout << (char)218 << (char)196;
+	for (i = 1; i < size; i++) std::cout << (char)194 << (char)196;
+	std::cout << (char)191 << "\n";
+	for (t = 0; t < size; t++) {
+			std::cout << (char)179;
+			for (i = 1; i <= size; i++) std::cout << board[t][i-1] << (char)179;
+			std::cout << "\n";
+			if (t == size - 1) break;
+			std::cout << (char)195<<(char)196;
+			for (i = 1; i < size; i++) std::cout << (char)197 << (char)196;
+			std::cout << (char)180 << "\n";
 		}
-		if (i == 0)
-			std::cout << (char)136;
-	}
+	std::cout << (char)192 << (char)196;
+	for (i = 1; i < size; i++) std::cout << (char)193 << (char)196;
+	std::cout << (char)217 << "\n";
 }
 bool trafficChoice(char**board, int size, int& choiceX, int& choiceY) {
 	if ((choiceX >= 0 && choiceX < size) && (choiceY >= 0 && choiceY < size)) {
 		if (board[choiceX][choiceY]) return true;
 	}
 	return false;
+}
+void prMainMenu(Program& program, int& choice,int& value,bool reply) {
+	reply = false;
+	do {
+		system("cls");
+		if (reply)
+			std::cout << "Podano niewlasciwe dane!\n";
+		std::cout << "|---Menu Glowne---|\n"
+			<< "1.\tGra w kolko i krzyzyk\n"
+			<< "2.\tWlasne ustawienia gry\n"
+			<< "3.\tWyjscie\n"
+			<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
+		std::cin >> choice;
+		reply = true;
+	} while (mainSelection(choice, value, program));
+}
+void prSizeMenu(Program& program, int& size,int& length, int& value, bool reply) {
+	reply = false;
+	do {
+		system("cls");
+		if (reply)
+			std::cout << "Podano niewlasciwe dane!\n";
+		std::cout << "|---Wybor opcji gry---|\n"
+			<< "Podaj rozmiar planszy: ";
+		std::cin >> size;
+		std::cout << "Podaj jak dluga linie trzeba ulozyc: ";
+		std::cin >> length;
+		reply = true;
+	} while (sizeSelection(size, length, value, program));
+}
+void prChoice(Program& program, int& choice, bool& wheel,int& value,bool reply) {
+	reply = false;
+	do {
+		system("cls");
+		if (reply)
+			std::cout << "Podano niewlasciwe dane!\n";
+		std::cout << "|--Menu wyboru znaku--|\n"
+			<< "1.\tO\n"
+			<< "2.\tX\n"
+			<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
+		std::cin >> choice;
+		reply = true;
+	} while (charSelection(choice, wheel, program));
+}
+void prVictoryMenu(Program& program,int& choice, int& value, bool reply ) {
+	reply = false;
+	do {
+		system("cls");
+		if (reply)
+			std::cout << "Podano niewlasciwe dane!\n";
+		std::cout << "Zwyciestwo!!\n"
+			<< "1.\tGraj jeszcze raz\n"
+			<< "2.\tWroc do Menu Glownego\n"
+			<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
+		reply = true;
+		std::cin >> choice;
+	} while (victorySelection(choice, value, program));
+}
+void prDefeatMenu(Program& program, int& choice, int& value, bool reply) {
+	reply = false;
+	do {
+		system("cls");
+		if (reply)
+			std::cout << "Podano niewlasciwe dane!\n";
+		std::cout << "Przegrales!!\n"
+			<< "1.\tGraj jeszcze raz\n"
+			<< "2.\tWroc do Menu Glownego\n"
+			<< "Wybierz jedna z opcji powyzej, podajac odpowiednia liczbe\n";
+		reply = true;
+		std::cin >> choice;
+	} while (defeatSelection(choice, value, program));
+}
+void prExit(bool& play) {
+	std::cout << "Wyjscie\n";
+	play = false;
 }
